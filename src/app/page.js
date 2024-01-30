b125159c234afbd1a0fd8acaf3ba5086394a7de0
Vincentius Arnold <vincentiusarnoldfridolin@gmail.com>
2024-01-30 16:01:21 +0700

    Initial Commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,152 @@
-import Image from "next/image";
+"use client"
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+import axios from "axios";
+import React, { useState } from "react";
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+function Home() {
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+    const [url, setUrl] = useState('');
+    const [data, setData] = useState(null);
+    const [progress, setProgress] = useState('');
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+    const handleLoadSong = async () => {
+        setProgress('');
+        setData(null);
+        try {
+            const res = await axios.post(process.env.NEXT_PUBLIC_API_URL+'/fetch', { 'url': url });
+            res.data?.songs_list.map(item => { item.status = "Not Downloaded Yet" });
+            setData({...res.data});
+        } catch (e) {
+            console.log(e);
+        }
+    }
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+    const handleStartDownload = async () => {
+        try {
+            const res = await fetch(process.env.NEXT_PUBLIC_API_URL+'/playlist', {
+                method: "POST",
+                headers: {
+                    Accept: 'text/event-stream',
+                },
+                body: JSON.stringify(data),
+            });
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+            const reader = res.body.getReader();
+
+            while (true) {
+                const { done, value } = await reader.read();
+                const d = {...data};
+                let x = new TextDecoder().decode(value);
+                if (!done) {
+                    if (x[0] === 's') {
+                        d.songs_list[d.songs_list.length - 1].status = "Download Complete";
+                        setProgress((prog) => prog = x);
+                    } else {
+                        if (x <= d.songs_list.length) d.songs_list[x-1].status = "Downloading";
+                        if (x != 1) d.songs_list[x-2].status = "Download Complete";
+                    }
+                    setData(d);
+                }
+
+                if (done) {
+                    break;
+                }
+
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    const startDownload = async () => {
+        try {
+            console.log(progress);
+            let res = await axios.get(process.env.NEXT_PUBLIC_API_URL+'/download?id='+progress, 
+                {
+                    'responseType': 'arraybuffer'
+                }
+            );
+
+            // Create a Blob from the response data
+            const blob = new Blob([res.data], { type: 'application/zip; charset=utf-8' });
+
+            // Create a download link
+            const url = window.URL.createObjectURL(blob);
+            const link = document.createElement('a');
+            link.href = url;
+            link.download = progress+'.zip';
+
+            // Append the link to the document
+            document.body.appendChild(link);
+
+            // Trigger the download
+            link.click();
+
+            // Remove the link from the document
+            document.body.removeChild(link);
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    window.onload = () => {};
+
+    return (
+        <main className="flex min-h-screen flex-col items-center p-24 bg-slate-950">
+            <h1 className="text-xl">MAAP BELOM RESPONSIVE</h1>
+            <div className="flex items-center gap-2">
+                <input onChange={(e) => setUrl(e.target.value)} type="text" name="url" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Enter Url.." />
+                <button onClick={handleLoadSong} className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Fetch</button>
+                <button disabled={data === null} onClick={handleStartDownload} className="text-white bg-blue-700 disabled:bg-gray-500 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Confirm</button>
+                <button disabled={progress[0] !== 's'} onClick={startDownload} className="text-white bg-orange-700 disabled:bg-gray-500 hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 focus:outline-none">Download</button>
+            </div>
+            <section className="my-3"/>
+            <div className="w-10/12">
+                {data?.songs_list &&
+                    <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                            <tr>
+                                <th scope="col" className="px-6 py-3">
+                                    #
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Cover
+                                </th>
+                                <th scope="col" className="px-6 w-1/2 py-3">
+                                    Title
+                                </th>
+                                <th scope="col" className="px-6 py-3">
+                                    Status
+                                </th>
+                            </tr>
+                        </thead>
+                    {data?.songs_list && 
+                        data?.songs_list?.map((item, key) => (
+                            <React.Fragment key={key}>
+                                <tbody>
+                                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                        <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                                            <p>{item?.id}</p>
+                                        </th>
+                                        <td className="px-6 py-4 w-1/4">
+                                            <img alt={key} className="w-full" src={item?.thumbnail} />
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            <p className="font-semibold text-lg">{item?.title}</p>
+                                        </td>
+                                        <td className="px-6 py-4">
+                                            <p>{item?.status}</p>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </React.Fragment>
+                        ))
+                    }
+                    </table>
+                }
+            </div>
+        </main>
+    );
 }
+
+export default Home; 
